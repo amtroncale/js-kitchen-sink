1a5242ccd0c35336ea23b803d8c270c206ecb209
Adam Troncale <amtroncale@Adams-MacBook-Pro.local>
2020-05-23 08:46:58 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+console.log('JS Kitchen Sink');
+console.log('---------------');
+//store my name as a string
+let myName = 'Adam';
+//store the number of states
+const numOfStates = 50;
+//store the sum of 5 + 4
+let sum = 5 + 4;
+
+//creates sayHello function to console.log a value
+function sayHello() {
+    console.log('Hello World!');
+}
+
+sayHello();
+
+//creates checkAge
+function checkAge() {
+
+}
